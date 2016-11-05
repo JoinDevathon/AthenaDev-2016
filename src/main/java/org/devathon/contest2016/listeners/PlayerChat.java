39f30bdb223f2b24bb5965f6a3024deba1527880
Athena222 <athena222dev@gmail.com>
2016-11-05 12:12:15 -0600

    Commit #4

--- src/main/java/org/devathon/contest2016/listeners/PlayerChat.java
+++ src/main/java/org/devathon/contest2016/listeners/PlayerChat.java
@@ -28,11 +28,45 @@ public class PlayerChat implements Listener {
     @EventHandler
     public void onAsyncPlayerChat(AsyncPlayerChatEvent e) {
 
-        Player player = (Player) e.getPlayer();
+        Player player = e.getPlayer();
 
         // add minecraft command (and maybe git, for fun?)
 
         if(PlayerInteract.enterUsername.contains(player.getUniqueId().toString())) {
+            if(e.getMessage().equalsIgnoreCase(Utils.getUsername(player))) {
+                e.setCancelled(true);
+                player.sendMessage(ChatColor.GREEN + "> " + ChatColor.DARK_GREEN + "Signing into " + player.getName() + "...");
+
+                if(Utils.getPassword(player).equals("none")) {
+                    player.sendMessage(ChatColor.GREEN + "> " + ChatColor.DARK_GREEN + "Couldn't find a password for user " + player.getName() + ", please register one using /terminal setPassword <password>.");
+                    player.sendMessage(ChatColor.GREEN + "> " + ChatColor.DARK_GREEN + "Current operation cancelled.");
+                    player.sendMessage(ChatColor.GREEN + "> _");
+                }
+                else {
+                    player.sendMessage(ChatColor.GREEN + "> " + ChatColor.DARK_GREEN + "Please enter your terminal password.");
+                    player.sendMessage(ChatColor.GREEN + "> _");
+                    PlayerInteract.enterUsername.remove(player.getUniqueId().toString());
+                    PlayerInteract.enterPassword.add(player.getUniqueId().toString());
+                }
+            }
+            else {
+                e.setCancelled(true);
+                player.sendMessage(ChatColor.GREEN + "> " + ChatColor.DARK_GREEN + "Invalid username!");
+            }
+        }
+
+        if(PlayerInteract.enterPassword.contains(player.getUniqueId().toString())) {
+            if(e.getMessage().equalsIgnoreCase(Utils.getPassword(player))) {
+
+            }
+            else {
+                e.setCancelled(true);
+                player.sendMessage(ChatColor.GREEN + "> " + ChatColor.DARK_GREEN + "Invalid password!");
+            }
+        }
+
+        if(TerminalCommand.setUsername.containsKey(player.getUniqueId().toString())) {
+            e.setCancelled(true);
             TerminalCommand.setUsername.put(player.getUniqueId().toString(), e.getMessage());
             player.sendMessage(ChatColor.GREEN + "> " + ChatColor.GREEN + "Are you sure you want to change your terminal username to " + e.getMessage() + "? (y or n)");
 
@@ -44,24 +78,28 @@ public class PlayerChat implements Listener {
                 player.sendMessage(ChatColor.GREEN + "> " + ChatColor.DARK_GREEN + "Cancelled current operation.");
                 TerminalCommand.setUsername.remove(player.getUniqueId().toString(), e.getMessage());
             }
-
         }
 
         if(e.getMessage().equalsIgnoreCase("y") || (e.getMessage().equalsIgnoreCase("yes") && (TerminalCommand.setUsername.containsKey(player.getUniqueId().toString())))) {
             yesOrNo = true;
         }
 
+        /*
         if(e.getMessage().equalsIgnoreCase("exit") || (e.getMessage().equalsIgnoreCase("cancel") && (PlayerInteract.enterUsername.contains(player.getUniqueId().toString())) || (PlayerInteract.enterPassword.contains(player.getUniqueId().toString())))) {
+            e.setCancelled(true);
             PlayerInteract.enterUsername.remove(player.getUniqueId().toString());
             PlayerInteract.enterPassword.remove(player.getUniqueId().toString());
             player.sendMessage(ChatColor.GREEN + "> " + ChatColor.DARK_GREEN + "Cancelled current operation.");
         }
+        */
 
         if(e.getMessage().equalsIgnoreCase("y") || (e.getMessage().equalsIgnoreCase("yes"))) {
+            e.setCancelled(true);
             player.sendMessage(ChatColor.GREEN + "> " + ChatColor.DARK_GREEN + "Completed operation.");
         }
 
         if(e.getMessage().equalsIgnoreCase("n") || (e.getMessage().equalsIgnoreCase("no"))) {
+            e.setCancelled(true);
             player.sendMessage(ChatColor.GREEN + "> " + ChatColor.DARK_GREEN + "Current operation cancelled.");
         }
     }
